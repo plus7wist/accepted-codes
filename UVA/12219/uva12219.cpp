274c9bf09a94ad87043ba01fc983888e04c7e82d
astralknight@sina.com <astralknight@163.com>
2017-06-20 14:00:30 +0800

    update file

--- UVA/12219/uva12219.cpp
+++ UVA/12219/uva12219.cpp
@@ -1,53 +1,81 @@
-//这题数组开小了UVA判的是WA？？
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long LL;
-const int INF=0x3f3f3f3f;
-const int maxm=5005;int n;int m;
-int father[maxm];int edges[maxm];int weight[maxm];
-int from[maxm];int to[maxm];
-int cmp(int a,int b){return weight[a]<weight[b];}
-int look(int x){return father[x]==x?x:father[x]=look(father[x]);}
-int solve()
+const int maxn=50005;
+char op[maxn<<3];//题意是最多50000个结点，不是字符串最长50000！开太小会RE
+int vis[maxn];int nc;
+char *p;int ks;
+struct nodes
 {
-    for(int i=0;i<m;i++) edges[i]=i;
-    sort(edges,edges+m,cmp);
-    int ans=INF;
-    for(int l=0;l<m;l++)
+    string s;//结点用string存可简化很多
+    int ls,rs;int nhash;
+    bool operator <(const nodes &rhs) const
     {
-        for(int i=0;i<=n;i++) father[i]=i;
-        int nn=n;
-        for(int r=l;r<m;r++)
-        {
-            int e=edges[r];int ft=look(from[e]);int tt=look(to[e]);
-            if(ft!=tt)
-            {
-                father[ft]=tt;
-                if(--nn==1)//合并了n-1次说明全部连通，计算l,r边之差
-                {
-                    ans=min(ans,abs(weight[edges[r]]-weight[edges[l]]));
-                    break;
-                }
-            }
-        }
+        if(nhash!=rhs.nhash) return nhash<rhs.nhash;
+        if(ls!=rhs.ls) return ls<rhs.ls;
+        return rs<rhs.rs;
+}
+}node[maxn];
+map<nodes,int>ids;//快速查找结点
+void print(int cur)
+{
+    if(vis[cur])//访问过，打印编号
+    {
+        printf("%d",cur+1);
+        return;
     }
-    if(ans==INF) ans=-1;
-    return ans;
+    vis[cur]=1;
+    printf("%s",node[cur].s.c_str());//string转char
+    if(node[cur].ls!=-1)//有子树
+    {
+        printf("(");
+        print(node[cur].ls);
+        printf(",");
+        print(node[cur].rs);
+        printf(")");
+    }
+}
+int build_tree()
+{
+    int u=nc++;
+    nodes &cur=node[u];
+    cur.ls=-1;cur.rs=-1;cur.nhash=0;cur.s="";
+    while(isalpha(*p))
+    {
+        cur.s.push_back(*p);
+        cur.nhash=cur.nhash*27+*p-'a'+1;//这个hash函数选择得不好会被卡数据
+        p++;
+    }
+
+    if(*p=='(')//有子树
+    {
+        //cout<<p<<endl;
+        p++;
+        cur.ls=build_tree();
+        p++;
+        cur.rs=build_tree();
+        p++;
+    }
+
+    if(ids.count(cur)!=0)//旧结点
+    {
+        u--,nc--;
+        return ids[cur];
+    }
+    return ids[cur]=u;//新增结点
 }
 int main()
 {
     //freopen("test.txt","r",stdin);
-    //freopen("out.txt","w",stdout);
-    int a,b,c;
-    while(scanf("%d%d",&n,&m)==2&&n)
+    int t;scanf("%d",&t);
+    for(ks=1;ks<=t;ks++)
     {
-        for(int i=0;i<m;i++)
-        {
-            scanf("%d%d%d",&a,&b,&c);
-            from[i]=a;to[i]=b;weight[i]=c;
-        }
-        int ans=solve();
-        printf("%d\n",ans);
+        nc=0;ids.clear();memset(vis,0,sizeof(vis));
+        scanf("%s",op);
+        p=op;//存放当前打印位置的全局指针
+        int u=build_tree();
+        print(u);
+        printf("\n");
     }
     return 0;
 }
